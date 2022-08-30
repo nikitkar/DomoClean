91342074737f718ec69d5636128c036ef6fdc960
Nikita <nikki-xit@mail.ru>
2022-08-31 02:11:39 +0300

    create header

--- functions.php
+++ functions.php
@@ -3,10 +3,10 @@ add_action('wp_enqueue_scripts', function(){
     wp_enqueue_style('reboot-style-child', get_stylesheet_uri(), array('reboot-style'));
     
 
-    wp_enqueue_style('reset', get_template_directory_uri() . '/assets/css/reset.css');
-    wp_enqueue_style('header', get_template_directory_uri() . '/assets/css/header.scss');
-    wp_enqueue_style('footer', get_template_directory_uri() . '/assets/css/footer.scss');
-    wp_enqueue_style('style', get_template_directory_uri() . '/assets/css/style.scss');
+    wp_enqueue_style('reset', get_template_directory_uri() . '/assets/style/reset.css');
+    wp_enqueue_style('header', get_template_directory_uri() . '/assets/style/header.css');
+    wp_enqueue_style('footer', get_template_directory_uri() . '/assets/style/footer.css');
+    wp_enqueue_style('style', get_template_directory_uri() . '/assets/style/style.css');
 });
 
 add_theme_support('post-thumbnails');
