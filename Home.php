1c0367a38442aecfd9e26cef624b20634b39e09d
Nikita <nikki-xit@mail.ru>
2022-08-31 21:37:14 +0300

    create commit test

--- Home.php
+++ Home.php
@@ -6,8 +6,47 @@ Template Name: Home
 
 <?php get_header(); ?>
 
-<div class="conteiner">
-    
-</div>
+<main class="main">
+    <section class="slider">
+        <div class="sliderWrapper">
+            <div class="sliderCarousel">
+                <div class="sliderInner">
+                    <div class="sliderItems">
+                        <div class="sliderItem">
+                            <img src="" alt="slider img fisrt">
+                        </div>
+                    </div>
+                    <div class="sliderItems">
+                        <div class="sliderItem">
+                            <img src="" alt="slider img second">
+                        </div>
+                    </div>
+                    <div class="sliderItems">
+                        <div class="sliderItem">
+                            <img src="" alt="slider img third">
+                        </div>
+                    </div>
+                    <div class="sliderItems">
+                        <div class="sliderItem">
+                            <img src="" alt="slider img fourth">
+                        </div>
+                    </div>
+                </div>
+                <div class="sliderBtn">
+
+                </div>
+                <div class="conteiner">
+                    <div class="sliderContent">
+                        <div class="sliderContent--decor">
+                            <img class="sliderContent--imgLogo" src="<?php bloginfo('template_url'); ?>/assets/images/logo/logo.png" alt="logo">
+                            <h2>Клининговые услуги <span>№1</span> в Казани</h2>
+                            <img class="sliderContent--bg" src="<?php bloginfo('template_url'); ?>/assets/images/slideContent--bg.png" alt="bg">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
 
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -12,5 +12,3 @@ add_action('wp_enqueue_scripts', function(){
 add_theme_support('post-thumbnails');
 add_theme_support('title-tag');
 add_theme_support('custom-logo');
-
-?>
