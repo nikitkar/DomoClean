1b2041d957472d3c707c9f9981eb318cbcf7fb17
Nikita <nikki-xit@mail.ru>
2022-09-01 13:41:05 +0300

    create info block and our services block

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $(".sliderInner").slick({
+        dots: true,
+        infinite: false,
+        arrows: false,
+        autoplay: false,
+        autoplaySpeed: 2000,
+    });
+});
