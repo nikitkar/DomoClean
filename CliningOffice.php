d70dec9a158ffac8d360f3ba2c8f0e54b49f262d
ipolomar <ipolomar@inbox.ru>
2022-09-03 23:47:01 +0300

    create page

---
+++ CliningOffice.php
@@ -0,0 +1,156 @@
+<?php
+/*
+Template Name: CliningOffice
+*/
+?>
+
+<?php get_header(); ?>
+
+<main class="main">
+    <section class="breadcrumbs--info">
+        <div class="conteiner">
+            <div class="breadcrumbsWrapper">
+                <div class="breadcrumbs">
+                    <a class="breadcrumbs--main" href="/">
+                        На главную</a>
+                    <span class="breadcrumbs--separator">/</span>
+                    <a class="breadcrumbs--child">
+                        Клининг офисов
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="mainWrapper">
+        <div class="conteiner">
+            <div class="mainWrapper--inner">
+                <section class="CleaningAfterRenovation">
+                    <div class="CleaningAfterRenovation--info">
+                        <h1 class="CleaningAfterRenovation--title">
+                            Уборка после ремонта
+                        </h1>
+                        <p class="CleaningAfterRenovation--text">
+                        Основная особенность уборки после ремонта или строительных работ сводится к масштабам. Самостоятельно справиться с этим, к примеру, если речь касается коттеджа, достаточно сложно. Даже квартиру после действий ремонтников или строительства ограждений, перегородок убирать трудно. Редкие бригады оставляют за собой совершенный порядок. По этой причине имеет место трудоемкость клининга. Клей, цемент, шпатлевка, следы других материалов – почти неизбежность. Нередко пыль в результате строительства оседает даже в труднодоступных местах.
+                        </p>
+                        <br>
+                        <p class="CleaningAfterRenovation--text">
+                        Сотрудники нашей клининговой компании обеспечивают качественное удаление любых загрязнений после ремонта дач, квартир, домов, офисных помещений. Гарантируем результативное мытье и удаление загрязнений в виде пятен герметика, силикона, затирки, скотча. Справимся даже со стойкой краской. Отмываем плинтусы, полы, стены, потолки.
+                        </p>
+                        <br>
+                        <p class="CleaningAfterRenovation--text">
+                        Специалисты компании «Домоклин» знают, как удалить 99% загрязнений, возникающих вследствие строительства. Это обеспечено опытом, профессиональными средствами в распоряжении наших работников. Мы можем гарантировать совершенный результат. В минимально возможный срок.
+                        </p>
+                        <br>
+                    </div>
+                </section>
+                <section class="WhyCleaning">
+                    <div class="WhyCleaning--info">
+                        <h2 class="WhyCleaning--title">
+                            Почему уборка после ремонта так сложна?
+                        </h2>
+                        <div class="WhyCleaningImg">
+                            <img class="WhyCleaningImg--img" src="<?php bloginfo('template_url'); ?>/assets/images/cliningOffice/as.png" alt="WhyCleaningImg">
+                        </div>
+                        <p class="WhyCleaning--text">
+                        Потому что возникают сложно поддающиеся выведению загрязнения. Примеры уже были представлены. С помощью обыкновенной генеральной уборки справиться с ними объективно невозможно.
+                        </p>
+                        <br>
+                        <p class="WhyCleaning--text">
+                        Вот факторы, определяющие особые сложности: 
+                        <br>
+                        Масштаб работ. Даже если дело касается косметического ремонта, загрязняются все поверхности в квартире или доме. В ситуации, когда все сводится к восстановлению эстетики только одного помещения, ничего не меняется. Пыль, иные взвеси неизбежно попадут в труднодоступные места. Попытки предупредить это редко приводят к результатам.
+                        <br>
+                        Сложные загрязнения. Если часть вещей и предметов можно укрыть, то стены, пол, некоторые другие элементы – нет. Всю квартиру защитить от грязи и пыли невозможно. Удаление сложных загрязнений на практике требует применения специальных средств, дополнительного оборудования. Потому как профессиональные составы и техника стоят дорого, приобретать их для разовых работ – нерациональное решение.
+                        <br>
+                        Вероятность получить ранения. Обычная уборка не сопряжена с какими-либо рисками. А после работ по ремонту можно наткнуться на острые предметы, колющие. В строительном мусоре их обычно слишком много. Потребуется использовать средства для индивидуальной защиты, но даже они не будут гарантировать абсолютную защищенность.
+                        <br>
+                        Чрезмерное содержание пыли в воздухе. Любая манипуляция с мусором, другими отходами после ремонта приводит к поднятию в воздух взвесей. Они не только объективно усложняют уборку, но и попадают в дыхательные пути. Это представляет опасность для здоровья человека, домашних животных.
+                        <br>
+                        Высокая вероятность порчи имущества. Речь о бытовых приборах, мебели. Все работы по уборке следует проводить максимально аккуратно со знанием дела. В иной ситуации вероятность повреждения мебели, порчи ремонта становится высокой.
+                        </p>
+                        <br>
+                        <p class="WhyCleaning--text">
+                            Помимо прочего клининг требует затрат времени. Но только не в случае с услугами клинеров.
+                        </p>
+                        <br>
+                    </div>
+                </section>
+                <section class="StandardServices">
+                    <div class="StandardServicesText">
+                        <h1 class="StandardServices--title">
+                            Стандартные услуги в рамках уборки после ремонта
+                        </h1>
+                        <p class="StandardServices--text">
+                        От потока людей в офисе зависит частота и требуемая регулярность уборки. Внимания заслуживают все служебные помещения вне зависимости от назначения. Представим основные виды уборки и кратко опишем их.
+                        </p>
+                    </div>
+                    <div class="StandardServicesBlock">
+                        <ul class="StandardServicesBlock--info">
+                            <li class="StandardServicesBlock--items">
+                                <div class="StandardServicesBlock--imgInner" style="background: url(<?php bloginfo('template_url'); ?>/assets/images/CliningOffice/circle.png); background-position: center; background-repeat: no-repeat; background-size: contain">
+                                    <img class="StandardServicesBlock--img" src="<?php bloginfo('template_url'); ?>/assets/images/CliningOffice/offerImg1.png" alt="offerItemImg1">
+                                </div>
+                                <h4 class="StandardServicesBlock--title">Очистка поверхностей</h4>
+                                <br>
+                                <p class="StandardServicesBlock--text">
+                                    Предполагается очистка от строительной пыли. Она является достаточно мелкой и без проблем проникает в труднодоступные места. Вычистить ее или вымыть сложно. Стандартные вакуумные пылесосы с задачей не справляются. Тряпки даже с качественными средствами – то же. Уже отмечено, что пыль плоха не только в аспектах загрязнения, но и из-за опасности для здоровья.
+                                </p>
+                                <br>
+                                <p class="StandardServicesBlock--text">
+                                    Наши специалисты задействуют мощный промышленный пылесос, чтобы избавить вас от строительной пыли. Мы обработаем все поверхности в несколько этапов. Это гарантия совершенной чистоты.
+                                </p>
+                            </li>
+                            <li class="StandardServicesBlock--items">
+                                <div class="StandardServicesBlock--imgInner" style="background: url(<?php bloginfo('template_url'); ?>/assets/images/CliningOffice/circle.png); background-position: center; background-repeat: no-repeat; background-size: contain">
+                                    <img class="StandardServicesBlock--img" src="<?php bloginfo('template_url'); ?>/assets/images/CliningOffice/offerImg1.png" alt="offerItemImg1">
+                                </div>
+                                <h4 class="StandardServicesBlock--title">Удаление пленки, скотча</h4>
+                                <br>
+                                <p class="StandardServicesBlock--text">
+                                    Внимания в данном контексте заслуживают окна. Их меняют в первую очередь. Стандартный метод защиты – использование пленки и скотча. Они в некоторых ситуациях в результате слишком плотного контакта со стеклом и пластиком буквально въедаются. Удаление объективно затруднено. Есть вероятность повреждений при попытке задействовать механический способ.
+                                </p>
+                                <br>
+                                <p class="StandardServicesBlock--text">
+                                    У нас есть решение. Мы будем применять специальные средства, инструменты, которые не повредят поверхность окон.
+                                </p>
+                            </li>
+                            <li class="StandardServicesBlock--items">
+                                <div class="StandardServicesBlock--imgInner" style="background: url(<?php bloginfo('template_url'); ?>/assets/images/CliningOffice/circle.png); background-position: center; background-repeat: no-repeat; background-size: contain">
+                                    <img class="StandardServicesBlock--img" src="<?php bloginfo('template_url'); ?>/assets/images/CliningOffice/offerImg1.png" alt="offerItemImg1">
+                                </div>
+                                <h4 class="StandardServicesBlock--title">Очистка от клея, краски, иных загрязнений всех поверхностей</h4>
+                                <br>
+                                <p class="StandardServicesBlock--text">
+                                    Даже если бригада проявляла исключительную аккуратность, загрязнения такого рода не исключены. Труднее всего удалять их с поверхностей, являющихся рельефными. Совершенный результат уборки обеспечивается благодаря сочетанию механического и химического способов. Мы будем действовать аккуратно. Ваши вещи и мебель будут в полной сохранности с нашими мастерами.
+                                </p>
+                            </li>
+                            <li class="StandardServicesBlock--items StandardServicesBlock--items--small">
+                                <div class="StandardServicesBlock--imgInner" style="background: url(<?php bloginfo('template_url'); ?>/assets/images/CliningOffice/circle.png); background-position: center; background-repeat: no-repeat; background-size: contain">
+                                    <img class="StandardServicesBlock--img" src="<?php bloginfo('template_url'); ?>/assets/images/CliningOffice/offerImg1.png" alt="offerItemImg1">
+                                </div>
+                                <h4 class="StandardServicesBlock--title">Удаление затирки с кафеля, сантехники</h4>
+                                <br>
+                                <p class="StandardServicesBlock--text">
+                                    После ремонтных работ плитка, кафель и сантехника характеризуются как испачканные затиркой. Частично ремонтники удаляют следы материала, но не более этого.
+                                </p>
+                            </li>
+                            <li class="StandardServicesBlock--items StandardServicesBlock--items--small">
+                                <div class="StandardServicesBlock--imgInner" style="background: url(<?php bloginfo('template_url'); ?>/assets/images/CliningOffice/circle.png); background-position: center; background-repeat: no-repeat; background-size: contain">
+                                    <img class="StandardServicesBlock--img" src="<?php bloginfo('template_url'); ?>/assets/images/CliningOffice/offerImg1.png" alt="offerItemImg1">
+                                </div>
+                                <h4 class="StandardServicesBlock--title">Вынос мусора (5 кг – максимум)</h4>
+                                <br>
+                                <p class="StandardServicesBlock--text">
+                                    Даже после вывоза крупногабаритного мусора квартира или дом все равно являются захламленными. Ощущение чистоты исключается. Команда «Домоклин» обеспечит вынос мусора. Вам останется только насладиться совершенной чистотой.
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php get_footer(); ?>
