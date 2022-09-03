e8f983b118f6fb5369f94ec856d61967a575ef12
Nikita <nikki-xit@mail.ru>
2022-09-03 20:13:10 +0300

    create questions block

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,20 @@ $(function () {
         autoplaySpeed: 2000,
     });
 });
+
+let acc = document.getElementsByClassName("itemBtn");
+
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        console.log(this.nextElementSibling)
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+            panel.style = "padding: 0;";
+        } else {
+            panel.style = "padding: 20px;";
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
