4f2d857801ca2eb62038b318031e9c17031dbe27
Nikita <nikki-xit@mail.ru>
2022-09-05 11:48:21 +0300

    creating footer

--- Footer.php
+++ Footer.php
@@ -1,11 +1,106 @@
         <footer class="footer">
             <div class="conteiner">
                 <div class="footerWrapper">
-                    <h1>footer function</h1>
+                    <ul class="footerList">
+                        <li class="footerItems">
+                            <img class="footerLogo" src="<?php bloginfo('template_url'); ?>/assets/images/logo/logoBold.png" alt="footerLogo">
+                        </li>
+                        <li class="footerItems">
+                            <a class="footerItem" href="javascript:void(0);">Информация</a>
+                            <a class="footerItem" href="javascript:void(0);">Акции и скидки</a>
+                            <a class="footerItem" href="javascript:void(0);">Сертификаты</a>
+                            <a class="footerItem" href="javascript:void(0);">Вакансии</a>
+                            <a class="footerItem" href="javascript:void(0);">Контакты</a>
+                            <a class="footerItem" href="javascript:void(0);">Статьи</a>
+                            <a class="footerItem" href="javascript:void(0);">Видео</a>
+                            <a class="footerItem" href="javascript:void(0);">Рекламодателям</a>
+                        </li>
+                        <li class="footerItems">
+                            <h4 class="footerItem--title">Основные услуги</h4>
+                            <a class="footerItem" href="javascript:void(0);">Уборка квартиры</a>
+                            <a class="footerItem" href="javascript:void(0);">Генеральная уборка</a>
+                            <a class="footerItem" href="javascript:void(0);">Уборка коттеджа</a>
+                            <a class="footerItem" href="javascript:void(0);">Уборка офиса</a>
+                            <a class="footerItem" href="javascript:void(0);">Уборка после ремонта</a>
+                        </li>
+                        <li class="footerItems">
+                            <a class="footerItem--tel" href="tel:+89003287500">
+                                <div class="footerItem--telImg">
+                                    <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <g clip-path="url(#clip0_871_6273)">
+                                            <path d="M21.9999 16.9616V19.9616C22.0011 20.2401 21.944 20.5158 21.8324 20.7709C21.7209 21.0261 21.5572 21.2552 21.352 21.4435C21.1468 21.6317 20.9045 21.7751 20.6407 21.8643C20.3769 21.9535 20.0973 21.9867 19.8199 21.9616C16.7428 21.6272 13.7869 20.5757 11.1899 18.8916C8.77376 17.3563 6.72527 15.3078 5.18993 12.8916C3.49991 10.2828 2.44818 7.3126 2.11993 4.2216C2.09494 3.94507 2.12781 3.66636 2.21643 3.40323C2.30506 3.14009 2.4475 2.89829 2.6347 2.69322C2.82189 2.48815 3.04974 2.32431 3.30372 2.21212C3.55771 2.09994 3.83227 2.04186 4.10993 2.0416H7.10993C7.59524 2.03682 8.06572 2.20868 8.43369 2.52513C8.80166 2.84159 9.04201 3.28105 9.10993 3.7616C9.23656 4.72167 9.47138 5.66433 9.80993 6.5716C9.94448 6.92952 9.9736 7.31852 9.89384 7.69248C9.81408 8.06645 9.6288 8.40971 9.35993 8.6816L8.08993 9.9516C9.51349 12.4551 11.5864 14.528 14.0899 15.9516L15.3599 14.6816C15.6318 14.4127 15.9751 14.2275 16.3491 14.1477C16.723 14.0679 17.112 14.0971 17.4699 14.2316C18.3772 14.5702 19.3199 14.805 20.2799 14.9316C20.7657 15.0001 21.2093 15.2448 21.5265 15.6191C21.8436 15.9934 22.0121 16.4712 21.9999 16.9616Z" fill="#4F4F4F" />
+                                            <path d="M15.05 5.0415C16.0268 5.23207 16.9244 5.70976 17.6281 6.41344C18.3318 7.11712 18.8095 8.01477 19 8.9915M15.05 1.0415C17.0793 1.26694 18.9716 2.17568 20.4163 3.61851C21.8609 5.06134 22.7721 6.95251 23 8.9815" stroke="#4F4F4F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_871_6273">
+                                                <rect width="24" height="24" fill="white" transform="translate(0 0.0415039)" />
+                                            </clipPath>
+                                        </defs>
+                                    </svg>
+
+                                </div>
+                                8 900 328 75 00
+                            </a>
+                            <a class="footerItem--workingTime" href="javascript:void(0);">
+                                <div class="footerItem--workingTimeImg">
+                                    <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M12 22.0415C17.5228 22.0415 22 17.5644 22 12.0415C22 6.51866 17.5228 2.0415 12 2.0415C6.47715 2.0415 2 6.51866 2 12.0415C2 17.5644 6.47715 22.0415 12 22.0415Z" stroke="#4F4F4F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                        <path d="M12 6.0415V12.0415L16 14.0415" stroke="#4F4F4F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </div>
+                                <div class="footerItem--workingTimeContent">
+                                    с 9:00 до 22:00<br>
+                                    <span>без выходных</span>
+                                </div>
+                            </a>
+                            <div class="footerItem--location" href="javascript:void(0);">
+                                <div class="footerItem--locationImg">
+                                    <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M3 9.0415L12 2.0415L21 9.0415V20.0415C21 20.5719 20.7893 21.0806 20.4142 21.4557C20.0391 21.8308 19.5304 22.0415 19 22.0415H5C4.46957 22.0415 3.96086 21.8308 3.58579 21.4557C3.21071 21.0806 3 20.5719 3 20.0415V9.0415Z" stroke="#4F4F4F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </div>
+
+                                <div class="footerItem--locationContent">
+                                    <p>Наш адрес:
+                                        <br>
+                                        Казань, ул. Глушко, 43
+                                        <br>
+                                    </p>
+                                    <a href="mailto:support@domoclean.ru">
+                                        support@domoclean.ruasd
+                                    </a>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                    <div class="footerPaymentMethod">
+                        <div class="footerPaymentMethod--privacyPolicy">
+                            <p>2022 @DOMOCLEAN.RU”</p>
+                            <a href="javascript:void(0);">Политика конфиденциальности</a>
+                        </div>
+                        <ul class="footerPaymentMethod--list">
+                            <li class="footerPaymentMethod--items">
+                                <img src="<?php bloginfo('template_url'); ?>/assets/images/footer/visa.svg" alt="PaymentMethodImg">
+                            </li>
+                            <li class="footerPaymentMethod--items">
+                                <img src="<?php bloginfo('template_url'); ?>/assets/images/footer/mastercard.svg" alt="PaymentMethodImg">
+                            </li>
+                            <li class="footerPaymentMethod--items">
+                                <img src="<?php bloginfo('template_url'); ?>/assets/images/footer/sberbank.svg" alt="PaymentMethodImg">
+                            </li>
+                            <li class="footerPaymentMethod--items">
+                                <img src="<?php bloginfo('template_url'); ?>/assets/images/footer/qiwi.svg" alt="PaymentMethodImg">
+                            </li>
+                            <li class="footerPaymentMethod--items">
+                                <img src="<?php bloginfo('template_url'); ?>/assets/images/footer/wallet.svg" alt="PaymentMethodImg">
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </footer>
-    </body>
+        </body>
 
-    <?php wp_footer();?>
-</html>
+        <?php wp_footer(); ?>
+
+        </html>
