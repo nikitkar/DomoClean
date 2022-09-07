e669bb02894cbb501b6be141c6897562421c6d75
Nikita <nikki-xit@mail.ru>
2022-09-07 21:39:28 +0300

    creating news pages

---
+++ cleaningApartmentsKazan.php
@@ -0,0 +1,161 @@
+<?php
+/*
+Template Name: cleaningApartmentsKazan
+*/
+?>
+
+<?php get_header(); ?>
+
+<main class="main">
+    <section class="breack">
+        <div class="conteiner">
+            <div class="breackWraper">
+                хлебные крошки
+            </div>
+        </div>
+    </section>
+
+    <div class="mainWrapper">
+        <div class="conteiner">
+            <div class="mainWrapper--inner">
+                <section class="cleaningApartments">
+                    <div class="cleaningApartments--wrapper">
+                        <h2 class="cleaningApartments--title">Уборка квартир в Казани</h2>
+
+                        <p class="cleaningApartments--text">
+                            Любой владелец квартиры желает видеть собственное жилье в чистоте, без которой невозможны комфорт, а также уют. Не всегда удается самостоятельно решать бытовые задачи, которые связаны с этим. Причина – современный и изнуряющий ритм жизни. Поддерживать жилые помещения в порядке удается далеко не каждому человеку.
+                            <br><br>
+                            <span>Мы готовы предложить вам услуги профессиональных уборщиков. Занимайтесь своими делами, пока мы обеспечиваем чистоту в вашей квартире. Или отдыхайте: с нами клининг – это просто, быстро, результативно.
+                            </span>
+                        </p>
+
+                        <section class="cleaningApartments--includedCleaning">
+                            <h3 class="cleaningApartments--includedCleaningTitle">Что входит в уборку</h3>
+
+                            <ul class="cleaningApartmentsList">
+                                <li class="cleaningApartmentsItems">
+                                    <div class="cleaningApartments--innerImg">
+                                        <img class="cleaningApartmentsImg" src="<?php bloginfo('template_url'); ?>/assets/images/cleaningKazan/cleaningKazanItemsImg1.png" alt="cleaningApartmentsImg">
+                                    </div>
+
+                                    <div class="cleaningApartmentsItem">
+                                        <h5 class="cleaningApartmentsItem--title">Комнаты, помещения</h5>
+                                        <ul class="cleaningApartmentsItem--list">
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Вакуумная сухая чистка пылесосом</p>
+                                            </li>
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Мытье пола</p>
+                                            </li>
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Обеспыливание мебели (диванов, кресел, шкафов (протираем снаружи)), бытовых приборов</p>
+                                            </li>
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Мойка окон, подоконников, дверей вместе с проемами, радиаторов отопления</p>
+                                            </li>
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Мойка или сухая чистка стен, зеркал, иных стеклянных поверхностей</p>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </li>
+
+                                <li class="cleaningApartmentsItems">
+                                    <div class="cleaningApartments--innerImg">
+                                        <img class="cleaningApartmentsImg" src="<?php bloginfo('template_url'); ?>/assets/images/cleaningKazan/cleaningKazanItemsImg1.png" alt="cleaningApartmentsImg">
+                                    </div>
+
+                                    <div class="cleaningApartmentsItem">
+                                        <h5 class="cleaningApartmentsItem--title">Кухня</h5>
+                                        <ul class="cleaningApartmentsItem--list">
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Уборка пола кухни пылесосом (вакуумная чистка)</p>
+                                            </li>
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Влажная уборка кухонных поверхностей, техники (плиты, фартука, микроволновой печи, холодильника (почистим снаружи))</p>
+                                            </li>
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Чистка, дезинфекция сантехники</p>
+                                            </li>
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Вынос мусора (5 кг максимум)</p>
+                                            </li>
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Влажная уборка окон, подоконников, дверей вместе с блоками</p>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </li>
+
+                                <li class="cleaningApartmentsItems">
+                                    <div class="cleaningApartments--innerImg">
+                                        <img class="cleaningApartmentsImg" src="<?php bloginfo('template_url'); ?>/assets/images/cleaningKazan/cleaningKazanItemsImg1.png" alt="cleaningApartmentsImg">
+                                    </div>
+
+                                    <div class="cleaningApartmentsItem">
+                                        <h5 class="cleaningApartmentsItem--title">Ванная, санузел</h5>
+                                        <ul class="cleaningApartmentsItem--list">
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Чистка пылесосом</p>
+                                            </li>
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Мойка стеклянных поверхностей, пола, стен</p>
+                                            </li>
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Чистка сантехники, обработка защищающими средствами (протираем также санузел, душевую кабину, ванну)</p>
+                                            </li>
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Вынос мусора</p>
+                                            </li>
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Протирка труднодоступных мест</p>
+                                            </li>
+
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Дополнительная обработка (дезинфекция)</p>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </li>
+
+                                <li class="cleaningApartmentsItems">
+                                    <div class="cleaningApartments--innerImg">
+                                        <img class="cleaningApartmentsImg" src="<?php bloginfo('template_url'); ?>/assets/images/cleaningKazan/cleaningKazanItemsImg1.png" alt="cleaningApartmentsImg">
+                                    </div>
+
+                                    <div class="cleaningApartmentsItem">
+                                        <h5 class="cleaningApartmentsItem--title">Коридор, прихожая</h5>
+                                        <ul class="cleaningApartmentsItem--list">
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Удаление пыли со всех поверхностей (2 м – максимальная высота)</p>
+                                            </li>
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Протирание предметов интерьера, декора (обувница, дверные блоки (с двух сторон), зеркала)</p>
+                                            </li>
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Чистка ковровых, других покрытий с помощью пылесоса (в том числе коврика у входа)</p>
+                                            </li>
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Мытье пола</p>
+                                            </li>
+                                            <li class="cleaningApartmentsItem--items">
+                                                <p>Вынос мусора</p>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </li>
+                            </ul>
+                        </section>
+
+                        <div class="typeCleaning">
+                            
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+
+</main>
+
+<?php get_footer(); ?>
